2b3ad912463bbd3134e60c8dccc2dc4addc2b0bc
BOTOVELO Fidelys <botovelofidelys@gmail.com>
2026-04-28 21:31:08 +0100

    api

--- public/js/feed.js
+++ public/js/feed.js
@@ -1,4 +1,4 @@
-const API = '/api';
+const API = "https://mini-social-u0yc.onrender.com/api";
 let currentUser = null;
 
 document.addEventListener('DOMContentLoaded', async () => {
--- public/js/messages.js
+++ public/js/messages.js
@@ -1,4 +1,4 @@
-const API = '/api';
+const API = "https://mini-social-u0yc.onrender.com/api";
 let currentUser = null;
 let activeContact = null;
 let pollInterval = null;
